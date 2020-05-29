76bbe76b6a9dc33720ad553c2ac7a811cf8e4f46
景 <>
2020-05-29 16:00:37 +0800

    feat: 增加读取配置项来决定是否上传到后台kafka

--- app/controller/web/report.ts
+++ app/controller/web/report.ts
@@ -110,7 +110,9 @@ export default class ReportController extends Controller {
       if (tokenObj && tokenObj.is_use === 1) {
         // TODO: 这个地方为具体的业务代码，可以删除
         // 而且最好挪到service中去 层级比较明确
-        await this.saveWebReportDataForMongodb(body, tokenObj);
+        if (body.needPushtoKafaka) {
+          await this.saveWebReportDataForMongodb(body, tokenObj);
+        }
         ctx.helper.success();
       } else {
         this.ctx.body = this.app.retError("token错误, 请在后台申请token");
